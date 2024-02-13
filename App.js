3143fa4688449f20f7bce239b8ac75faa97ff732
Caifang Ruan <caifangruan@Caifangs-MacBook-Pro.local>
2024-02-12 23:37:17 -0800

    updated file

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import 'react-native-get-random-values';
+import React, { useState, useMemo } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ActivitiesContext from './components/ActivitiesContext'; 
+import StartScreen from './screens/StartScreen';
+import AllActivitiesScreen from './screens/AllActivitiesScreen';
+import SpecialActivitiesScreen from './screens/SpecialActivitiesScreen';
+import AddActivityScreen from './screens/AddActivityScreen';
+import { ActivitiesProvider } from './components/ActivitiesContext';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <Tab.Navigator>
+      <Tab.Screen name="All Activities" component={AllActivitiesScreen} />
+      <Tab.Screen name="Special Activities" component={SpecialActivitiesScreen} />
+    </Tab.Navigator>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  const [activities, setActivities] = useState([]);
+
+  const addActivity = (newActivity) => {
+    setActivities((currentActivities) => [...currentActivities, newActivity]);
+  };
+
+  const providerValue = useMemo(() => ({ activities, setActivities, addActivity }), [activities]);
+
+  return (
+    <ActivitiesContext.Provider value={providerValue}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Start" component={StartScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="MainTab" component={MainTabNavigator} options={{ headerShown: false }}/>
+          <Stack.Screen name="Add An Activity" component={AddActivityScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ActivitiesContext.Provider>
+  );
+};
+
+export default App;
