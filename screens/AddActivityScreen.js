fe5bca7277a5dfeb0284e1092b8ecbcb555a716e
Caifang Ruan <caifangruan@Caifangs-MacBook-Pro.local>
2024-02-26 22:22:31 -0800

    updated AddActivityScreen.js

--- screens/AddActivityScreen.js
+++ screens/AddActivityScreen.js
@@ -1,11 +1,12 @@
 import InputField from '../components/InputField';
-import React, { useState, useContext } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { View, StyleSheet, Button, Alert, Platform, Text, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Alert, Platform, Text } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import ActivitiesContext from '../components/ActivitiesContext'; 
 import { colors, spacing } from '../components/styles'; 
+import PressableButton from '../components/PressableButton';
+import { writeToDB } from '../Firebase/Firebase-helper';
+
 
 const AddActivityScreen = ({ navigation }) => {
   const [activityType, setActivityType] = useState(null);
@@ -14,34 +15,38 @@ const AddActivityScreen = ({ navigation }) => {
   const [isActivityPickerOpen, setIsActivityPickerOpen] = useState(false);
   const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
   
-  const { addActivity } = useContext(ActivitiesContext);
-
-  // Handle saving the new activity
+  // Handle saving the new activit
   const handleSave = () => {
     if (!activityType || isNaN(duration) || duration <= 0) {
       Alert.alert('Error', 'Please fill in all fields and ensure the data is valid.');
       return;
     }
-
+    else {
     // Create a new activity object with a unique ID 
     //and add it using the context
-    const durationNum = Number(duration);
-    const newActivity = {
-      id: uuidv4(), 
-      type: activityType,
-      duration: durationNum,
-      date: date, 
-    };
-    addActivity(newActivity);
+    OnTextEnterDB();
     navigation.goBack();
     }
+  }
 
-    // Handle date change from the date picker
+  // Handle date change from the date picker
   const handleDateChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
-    setIsDatePickerVisible(Platform.OS === 'ios'); 
+    setIsDatePickerVisible(false);
     setDate(currentDate);
   };
+
+  function OnTextEnterDB() {
+    const durationNum = Number(duration);
+    const newActivity = {
+      activityType: activityType,
+      duration: durationNum,
+      date: date.toDateString(), 
+      isSpecial: (activityType == 'Running' || activityType == 'Weights') && duration > 60,
+    };
+    writeToDB(newActivity);
+  }
+
 
   return (
     <View style={styles.container}>
@@ -77,10 +82,15 @@ const AddActivityScreen = ({ navigation }) => {
       <View style={{ height: 16 }} />
       
       <Text style={styles.label}>Date *</Text>
-      <TouchableOpacity style={styles.dateInput} onPress={() => setIsDatePickerVisible(true)}>
+      <PressableButton customizedStyle={{
+           padding: 10,
+           backgroundColor: colors.secondary,
+          }} 
+        buttonPressed={() => setIsDatePickerVisible(true)}>
         <Text>{date.toDateString()}</Text>
-      </TouchableOpacity>
+      </PressableButton>
       {isDatePickerVisible && (
+      
         <DateTimePicker
           value={date}
           mode="date"
@@ -89,9 +99,30 @@ const AddActivityScreen = ({ navigation }) => {
           maximumDate={new Date()}
         />
       )}
+      <View style={{ height: 60}} />
       <View style={styles.buttonContainer}>
-        <Button title="Cancel" onPress={() => navigation.goBack()} />
-        <Button title="Save" onPress={handleSave} />
+      <PressableButton
+          customizedStyle={{
+            backgroundColor:'red',
+            height: 35,
+            width: 100,
+          }}
+          buttonPressed={()=>navigation.goBack()}
+        >
+          <Text>Cancel</Text>
+      </PressableButton>
+      <PressableButton
+          customizedStyle={{
+            backgroundColor:'blue',
+            height: 35,
+            width: 100,
+            
+          }}
+          buttonPressed={() => handleSave()}
+        >
+          <Text>Save</Text>
+        </PressableButton>
+       
       </View>
     </View>
   );
