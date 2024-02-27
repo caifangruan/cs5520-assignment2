f155fd9d61b2c2104f9d77a80d672cb65a1d366b
Caifang Ruan <caifangruan@Caifangs-MacBook-Pro.local>
2024-02-26 22:13:18 -0800

    add CardComponent.js file

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,21 +1,6 @@
 import { View, StyleSheet, Platform } from "react-native";
 import React from "react";
 
-/**
- * This is the card component that will be used
- * @param props.flexDirection : the flexDirection of the card
- * @param props.justifyContent : the justifyContent of the card
- * @param props.alignItems : the alignItems of the card
- * @param props.color : the background color of the card
- * @param props.width : the width of the card
- * @param props.height : the height of the card
- * @param props.radius : the border radius of the card
- * @param props.marginTop : the margin Top of the card
- * @param props.marginLeft : the margin Left of the card
- * @param props.marginBottom : the margin Bottom of the card
- * @param props.marginRight : the margin Right of the card
- * @returns the card configuration
- */
 const CardComponent = (props) => {
   return (
     <View
